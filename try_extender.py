ba81eeba449695ab20e293bd0c2b84503d4626c0
Alice Scarpa <alicescarpa@gmail.com>
2015-07-24 18:39:20 -0300

    Use memory saving mode

--- try_extender.py
+++ try_extender.py
@@ -1,11 +1,16 @@
-from flask import Flask, request, jsonify
+import string
+
+from flask import Flask, request, jsonify, redirect
 from urllib import unquote
 
 from mozci.mozci import trigger_job
+from mozci.sources.allthethings import list_builders
+from mozci.utils.transfer import MEMORY_SAVING_MODE
 from revision_info import jobs_per_revision
 
 app = Flask(__name__, static_folder='static', static_url_path='/static')
-app.debug = True
+MEMORY_SAVING_MODE = True
+#app.debug = True
 
 
 @app.route("/backend/process_data", methods=['POST'])
@@ -13,6 +18,11 @@ def process_data():
     buildernames = map(unquote, request.form.keys())
     buildernames.remove('commit')
     commit = request.form['commit']
+
+    assert all(c in string.hexdigits for c in commit)
+    for buildername in buildernames:
+        assert buildername in list_builders()
+
     for buildername in buildernames:
         trigger_job(buildername=buildername, revision=commit, dry_run=True)
     return jsonify(request.form)
@@ -20,7 +30,13 @@ def process_data():
 @app.route("/backend/get_json")
 def get_json():
     commit = request.values['commit']
+    assert all(c in string.hexdigits for c in commit)
     return jsonify(jobs_per_revision(commit))
 
+@app.route("/")
+def index():
+    return redirect("/static/json_test.html")
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=8080)
